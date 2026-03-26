daaa67a50fa75ccf9a31686b3b51493f5941e447
ahmedtaboubi <52833532+ahmedtaboubi@users.noreply.github.com>
2026-03-26 16:03:45 +0100

    Change image paths to relative in squad-data.js
    Updated image paths to use relative URLs.

--- assets/js/squad-data.js
+++ assets/js/squad-data.js
@@ -4,14 +4,14 @@ const squadMembers = [
         role: "Comédien / Intelligence Collective",
         desc: "Met en scène vos équipes pour révéler les dynamiques cachées.",
         expertises: ["Team Building", "Créativité", "Cohésion"],
-        image: "assets/img/AHMED_TABOUBI.jpg"
+        image: "../assets/img/AHMED_TABOUBI.jpg"
     },
     {
         name: "MUSTAFA FYTEH",
         role: "Comédien",
         desc: "Gestion des émotions, utilise les techniques d'acteur pour débloquer les conflits",
         expertises: ["Leadership", "Incertitude", "Prise de Parole"],
-        image: "assets/img/WhatsApp Image 2026-03-25 at 19.48.50.jpeg"
+        image: "../assets/img/WhatsApp Image 2026-03-25 at 19.48.50.jpeg"
     }
 
 
